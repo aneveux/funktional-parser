4315a3db8cf31d20e73eb81d2f8df59caf76f29c
Antoine Neveux <aneveux@protonmail.com>
2020-04-01 16:13:15 +0200

    :sparkles: adding the parser type declaration

--- src/main/kotlin/com/github/aneveux/funktional/parser.kt
+++ src/main/kotlin/com/github/aneveux/funktional/parser.kt
@@ -1,2 +1,20 @@
 package com.github.aneveux.funktional
 
+/*
+    Funktional Parser:
+
+    Kotlin implementation of a functional parser,
+    based on the presentation and explanations from the
+    Professor Graham Hutton explanations in this Youtube
+    video: https://youtu.be/dDtZLm7HIJs.
+
+    The parser type declaration is made upon the professor's
+    explanations of what a parser is and what it should return.
+ */
+
+data class ParsingResult<T>(val parsed: T, val unparsed: String) {
+    override fun toString() = "(parsed=$parsed, unparsed=$unparsed)"
+}
+
+typealias Parser<T> = (String) -> List<ParsingResult<T>>
+
